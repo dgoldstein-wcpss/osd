59230e47f84f98c486e3e91659d9618c40b04863
Dave Goldstein <70542369+dgoldstein-wcpss@users.noreply.github.com>
2025-06-23 10:20:04 -0400

    Update wcpssstart.ps1

--- wcpssstart.ps1
+++ wcpssstart.ps1
@@ -41,7 +41,7 @@ if ($ImageFileItem){
         $Global:MyOSDCloud.ImageFileItem = $ImageFileItem
         $Global:MyOSDCloud.ImageFileName = $ImageFileName
         $Global:MyOSDCloud.ImageFileFullName = $ImageFileFullName
-        $Global:MyOSDCloud.OSImageIndex = 1 
+        $Global:MyOSDCloud.OSImageIndex = 1
     }
 }
 #endregion Custom Image
