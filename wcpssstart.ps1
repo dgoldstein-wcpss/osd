6207c594c1c3349a53e30569a65c83e9bc74ece4
Dave Goldstein <70542369+dgoldstein-wcpss@users.noreply.github.com>
2026-05-04 14:30:16 -0400

    Update ESD name for Windows 11 image

--- wcpssstart.ps1
+++ wcpssstart.ps1
@@ -30,7 +30,7 @@ $Global:MyOSDCloud = [ordered]@{
 
 #Testing Custom Images - Use this if you want to automate using your own WIM / ESD file
 #Region Custom Image
-$ESDName = 'Windows 11 25H2-Education.wim'
+$ESDName = 'Windows-11-Education.wim'
 $ImageFileItem = Find-OSDCloudFile -Name $ESDName  -Path '\OSDCloud\OS\'
 if ($ImageFileItem){
     $ImageFileItem = $ImageFileItem | Where-Object {$_.FullName -notlike "C*"} | Where-Object {$_.FullName -notlike "X*"} | Select-Object -First 1
